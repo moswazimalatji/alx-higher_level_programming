87bee59911ce21dc7816589d7ebdc91312836f4f
Moswazi Petrus Malatji <42552218+moswazimalatji@users.noreply.github.com>
2023-01-18 01:18:10 +0200

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
     text = r.text
     print("Body response:")
-    print("- type: {}".format(type(text)))
-    print("- content: {}".format(text))
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
